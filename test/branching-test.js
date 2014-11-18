a4ac0a73f6a779dc60ea105f6b873d04465fe7d4
Fedor Indutny <fedor@indutny.com>
2014-11-18 17:49:53 +0300

    branching: far jump, setcc

--- test/branching-test.js
+++ test/branching-test.js
@@ -52,7 +52,6 @@ describe('Disasm', function() {
       call r15
     */});
 
-    // TODO(indutny): jl, set, cmov
     test('cmov', function() {
       jc.forEach(function(cond) {
         this.cmov(cond, 'rax', 'rbx');
@@ -74,6 +73,56 @@ describe('Disasm', function() {
       cmov nl, rax, rbx
       cmov le, rax, rbx
       cmov nle, rax, rbx
+    */});
+
+    test('far-jcc', function() {
+      var l = this.label();
+      this.bind(l);
+      this.jl(l);
+      jc.forEach(function(cond) {
+        this.jl(cond, l);
+      }, this);
+    }, function() {/*
+      jmp -0x5
+      far-jcc o, -0xb
+      far-jcc no, -0x11
+      far-jcc b, -0x17
+      far-jcc nb, -0x1d
+      far-jcc z, -0x23
+      far-jcc nz, -0x29
+      far-jcc be, -0x2f
+      far-jcc nbe, -0x35
+      far-jcc s, -0x3b
+      far-jcc ns, -0x41
+      far-jcc p, -0x47
+      far-jcc np, -0x4d
+      far-jcc l, -0x53
+      far-jcc nl, -0x59
+      far-jcc le, -0x5f
+      far-jcc nle, -0x65
+    */});
+
+    test('set', function() {
+      jc.forEach(function(cond) {
+        this.set(cond, 'r9');
+      }, this);
+    }, function() {/*
+      setcc o, r9
+      setcc no, r9
+      setcc b, r9
+      setcc nb, r9
+      setcc z, r9
+      setcc nz, r9
+      setcc be, r9
+      setcc nbe, r9
+      setcc ns, r9
+      setcc ns, r9
+      setcc p, r9
+      setcc np, r9
+      setcc l, r9
+      setcc nl, r9
+      setcc le, r9
+      setcc nle, r9
     */});
   });
 });
