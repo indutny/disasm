f03874e831d56924b11bf53e769d007d3f138a47
Fedor Indutny <fedor@indutny.com>
2015-09-24 16:24:33 -0400

    x64: `vroundss`

--- test/fp-test.js
+++ test/fp-test.js
@@ -14,6 +14,16 @@ describe('Disasm', function() {
       vmovsd [r9, 0x8], xmm1
     */});
 
+    test('movss', function() {
+      this.movss('xmm1', 'xmm2');
+      this.movss('xmm1', [ 'r9', 8 ]);
+      this.movss([ 'r9', 8 ], 'xmm1');
+    }, function() {/*
+      vmovss xmm1, xmm2
+      vmovss xmm1, [r9, 0x8]
+      vmovss [r9, 0x8], xmm1
+    */});
+
     test('movq', function() {
       this.movq('xmm1', 'r9');
       this.movq('r9', 'xmm1');
@@ -28,12 +38,34 @@ describe('Disasm', function() {
       vmovq xmm3, [r9, 0x8]
     */});
 
+    test('movd', function() {
+      this.movd('xmm1', 'rcx');
+      this.movd('rcx', 'xmm1');
+      this.movd('xmm2', 'xmm1');
+      this.movd([ 'rcx', 8 ], 'xmm1');
+      this.movd('xmm3', [ 'rcx', 8 ]);
+    }, function() {/*
+      vmovd xmm1, ecx
+      vmovd ecx, xmm1
+      vmovq xmm2, xmm1
+      vmovd [ecx, 0x8], xmm1
+      vmovd xmm3, [ecx, 0x8]
+    */});
+
     test('cvtsd2si', function() {
       this.cvtsd2si('r9', 'xmm1');
       this.cvtsd2si('r14', [ 'r15', 5 ]);
     }, function() {/*
       vcvtsd2si r9, xmm1
       vcvtsd2si r14, [r15, 0x5]
+    */});
+
+    test('cvtss2si', function() {
+      this.cvtss2si('r9', 'xmm1');
+      this.cvtss2si('r14', [ 'r15', 5 ]);
+    }, function() {/*
+      vcvtss2si r9, xmm1
+      vcvtss2si r14, [r15, 0x5]
     */});
 
     test('cvttsd2si', function() {
@@ -44,12 +76,28 @@ describe('Disasm', function() {
       vcvttsd2si r14, [r15, 0x5]
     */});
 
+    test('cvttss2si', function() {
+      this.cvttss2si('r9', 'xmm1');
+      this.cvttss2si('r14', [ 'r15', 5 ]);
+    }, function() {/*
+      vcvttss2si r9, xmm1
+      vcvttss2si r14, [r15, 0x5]
+    */});
+
     test('cvtsi2sd', function() {
       this.cvtsi2sd('xmm1', 'r9');
       this.cvtsi2sd('xmm3', [ 'r15', 5 ]);
     }, function() {/*
       vcvtsi2sd xmm1, r9
       vcvtsi2sd xmm3, [r15, 0x5]
+    */});
+
+    test('cvtsi2ss', function() {
+      this.cvtsi2ss('xmm1', 'r9');
+      this.cvtsi2ss('xmm3', [ 'r15', 5 ]);
+    }, function() {/*
+      vcvtsi2ss xmm1, r9
+      vcvtsi2ss xmm3, [r15, 0x5]
     */});
 
     test('roundsd', function() {
@@ -64,6 +112,18 @@ describe('Disasm', function() {
       vroundsd xmm3, xmm1, 0x3
     */});
 
+    test('roundss', function() {
+      this.roundss('nearest', 'xmm3', 'xmm1');
+      this.roundss('down', 'xmm3', 'xmm1');
+      this.roundss('up', 'xmm3', 'xmm1');
+      this.roundss('zero', 'xmm3', 'xmm1');
+    }, function() {/*
+      vroundss xmm3, xmm1, 0x0
+      vroundss xmm3, xmm1, 0x1
+      vroundss xmm3, xmm1, 0x2
+      vroundss xmm3, xmm1, 0x3
+    */});
+
     test('ucomisd', function() {
       this.ucomisd('xmm1', 'xmm2');
       this.ucomisd('xmm1', [ 'r8', 3 ]);
@@ -72,9 +132,22 @@ describe('Disasm', function() {
       vucomisd xmm1, [r8, 0x3]
     */});
 
+    test('ucomiss', function() {
+      this.ucomiss('xmm1', 'xmm2');
+      this.ucomiss('xmm1', [ 'r8', 3 ]);
+    }, function() {/*
+      vucomiss xmm1, xmm2
+      vucomiss xmm1, [r8, 0x3]
+    */});
+
     testFpBinary('addsd');
     testFpBinary('mulsd');
     testFpBinary('subsd');
     testFpBinary('divsd');
+
+    testFpBinary('addss');
+    testFpBinary('mulss');
+    testFpBinary('subss');
+    testFpBinary('divss');
   });
 });
